8979e6d40a03787b50ccfca88c4dbf9488ebc3e1
Patryk Winter <patrykwintersapps@gmail.com>
2020-11-28 18:59:51 +0100

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Patryk Winter on 28/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
